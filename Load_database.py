031a6e6fa101ef2f56ffa48d6b87c7feb54f8824
L.Blackhurst <l.blackhurst19@imperial.ac.uk>
2020-04-01 20:09:01 +0100

    intial database connection (local)

--- Load_database.py
+++ Load_database.py
@@ -1,6 +1,47 @@
-import matplotlib.pyplot as plt
-import numpy as np
+import sqlite3
+import pathlib
+import urllib.parse
+import pandas as pd
+import numpy as np 
 
-x = np.linspace(0, 20, 100)  # Create a list of evenly-spaced numbers over the range
-plt.plot(x, np.sin(x))       # Plot the sine of each x point
-plt.show()                   # Display the plot
+from sqlite3 import Error
+
+# Location of database **Test**
+path = r"C:\Users\Loz\github\Test DB\2016-01-25_21-46-14_014.db"
+
+def _path_to_uri(path):
+    """ change path to URI compatability
+        path = location of database """
+    path = pathlib.Path(path)
+    if path.is_absolute():
+        return path.as_uri()
+    return 'file:' + urllib.parse.quote(path.as_posix(), safe=':/')
+
+# Make path read/write only so a database won't be created in error
+path = _path_to_uri(path) + '?mode=rw'
+
+def create_connection(db_file):
+    """ create a database connection to a SQLite database """
+    
+    try:
+        conn = sqlite3.connect(path, uri = True)
+        print("Connection successful")
+    
+    except Error as e:
+        print("An error occurred: ", e)  
+    
+    return conn   
+ 
+if __name__ == '__main__':
+    con = create_connection(path)
+
+cur = con.cursor()
+
+cur.execute("select * from ROI_4")
+
+print(cur.fetchall())
+
+con.close()
+    
+ 
+ 
