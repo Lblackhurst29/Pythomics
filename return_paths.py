5bf19f36f7766c0ef2485f872fbd20b5121b3174
L.Blackhurst <l.blackhurst19@imperial.ac.uk>
2020-04-28 18:10:18 +0100

    tidied up

--- return_paths.py
+++ return_paths.py
@@ -49,11 +49,3 @@ def list_paths(metadata, index_file, user_dir):
     merge_df.insert(0, 'id', merge_df['file_name'].str.slice(0,26,1) + '|' + merge_df['region_id'].map('{:02d}'.format))
     
     return merge_df
-
-meta = r"C:\Users\Loz\github\Test_DB\2020-03-05_cameratest.csv"
-
-index = r"C:\Users\Loz\github\auto_generated_results\index2.txt"
-
-user_dir = r"C:\Users\Loz\github\auto_generated_results"
-
-print(list_paths(meta, index, user_dir))
